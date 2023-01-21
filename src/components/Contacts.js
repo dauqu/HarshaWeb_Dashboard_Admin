6aa16495ef0336fd2495381e4832201dea7f48af
dauqu <sujeetsingh734321@gmail.com>
2023-01-21 15:53:54 +0530

    table2

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -60,40 +60,79 @@ const getContacts = () => {
       <Navbar toggleSidebar={toggleSidebar} />
       <div className="flex w-full h-full overflow-hidden flex-row">
         <Sidebar status={sidebarStatus} />
-        <div className={`lg:w-[82%] w-[100%]  bg-[#F7FAFC] overflow-y-auto pb-6`}>
-            <div className="xl:w-[1000px] lg:w-[700px] md:w-[500px] sm:w-[500px] w-full mx-auto my-10 contacts">
-              <h1 className="text-center my-6 text-[30px] font-semibold">Contacts</h1>
-              <div className="overflow-x-scroll">
-                <table className="w-full">
-                  <thead className="w-full h-[50px]">
-                    <tr className="w-full text-left text-[#464A53]">
-                      <th className="px-4 py-2 border">First Name</th>
-                      <th className="px-4 py-2 border">Last Name</th>
-                      <th className="px-4 py-2 border">Email</th>
-                      <th className="px-4 py-2 border">Phone</th>
-                      <th className="px-4 py-2 border">Service</th>
-                      <th className="px-4 py-2 border">Subject</th>
-                      <th className="px-4 py-2 border">Message</th>
-                      <th className="px-4 py-2 border">Date-Time</th>
-                      {/* <th className="px-4 py-2 border">Action</th> */}
+        
+        <div className="container mx-auto px-4 sm:px-8">
+          <div className="py-8">
+            <div className="flex w-full">
+              <h2 className="md:text-2xl text-[18px] font-semibold">View Application Requests</h2>
+            </div>
+            <hr className="my-5 border-gray-400" />
+
+            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div className="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+                <table className="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">First Name</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Last Name</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Email</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Phone</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Service</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Subject</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Message</th>
+                      <th className="px-3 py-4 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">DateTime</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {contacts.length > 0 && contacts.map((val, key) => {
+                  {contacts.length > 0 && contacts.map((val, key) => {
                       return (
                         <>
-                          <tr className="text-left text-[#272727] text-[14px] h-[45px]">
-                            <td className="px-4 py-2 border text-[#008eff]">
-                              {val.fname}
-                            </td>
-                            <td className="px-4 py-2 border">{val.lname}</td>
-                            <td className="px-4 py-2 border">{val.email}</td>
-                            <td className="px-4 py-2 border">{val.phone}</td>
-                            <td className="px-4 py-2 border">{val.service}</td>
-                            <td className="px-4 py-2 border">{val.subject}</td>
-                            <td className="px-4 py-2 border">{val.message}</td>
-                            <td className="px-4 py-2 border">{val.datetime}</td>
-                          </tr>
+                        <tr key={item._id}>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <div className="flex">
+                              <div className="ml-3">
+                                <p className="text-gray-900 text-[16px]    whitespace-no-wrap">
+                                {val.fname}
+                                </p>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.lname}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px] whitespace-no-wrap">
+                            {val.email}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.phone}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.service}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.subject}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.message}
+                            </p>
+                          </td>
+                          <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 text-[16px]  whitespace-no-wrap">
+                            {val.datetime}
+                            </p>
+                          </td>
+                        </tr>
                         </>
                       );
                     })}
@@ -101,6 +140,8 @@ const getContacts = () => {
                 </table>
               </div>
             </div>
+          </div>
+          
         </div>
       </div>
     </div>
